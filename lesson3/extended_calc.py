5b98872641af7e1652e40169ec82cb795fa28ff4
clipklop <siliconchik@gmail.com>
2017-11-24 06:59:38 +0300

    calculator implementation is finished, little bit wierd, but it works

--- lesson3/extended_calc.py
+++ lesson3/extended_calc.py
@@ -1,28 +1,41 @@
-#
-def calc(string):
-    string = string.lower().replace(" ", "")
-    parts = string.split("+")
+# **
+# An advanced calculator.
+# **
 
-    for plus in range(len(parts)):
-        if "-" in parts[plus]:
-            parts[plus] = parts[plus].split("-")
 
-    print(parts)
+def calculator(string):
+    """Main calculator function."""
+    try:
+        string = string.replace(' ', '')
+        parts = string.split("+")
 
-    for plus in range(len(parts)):
-        parts[plus] = precalc(parts[plus])
+        for plus in range(len(parts)):
+            if '-' in parts[plus]:
+                parts[plus] = parts[plus].split('-')
+        print(parts)
 
-    print(parts)
+        for plus in range(len(parts)):
+            parts[plus] = precalc(parts[plus])
+        print(parts)
+
+        result = sum(parts)
+    except ValueError:
+        result = 'Something goes wrong.'
+    except ZeroDivisionError:
+        result = "Don't break the university."
+    return result
+
 
 def precalc(part):
+    """Split and calculate nested integers in a list by its operand."""
     if type(part) is str:
-        if "*" in part:
+        if '*' in part:
             result = 1
-            for subpart in part.split("*"):
-                result *= float(subpart)
+            for subpart in part.split('*'):
+                result *= precalc(subpart)
             return result
-        elif "/" in part:
-            parts = list(map(float, part.split("/")))
+        elif '/' in part:
+            parts = list(map(precalc, part.split("/")))
             result = parts[0]
             for subpart in parts[1:]:
                 result /= subpart
@@ -32,10 +45,10 @@ def precalc(part):
     elif type(part) is list:
         for i in range(len(part)):
             part[i] = precalc(part[i])
+        return part[0] - sum(part[1:])
+
     return part
 
 
 if __name__ == "__main__":
-    calc("3 / 2 + 10 + 2 * 3 * 2.5 - 4 + 1 / 2")
-    print(precalc("6*3"))
-    print(precalc("1/2"))
+    print(calculator("3 / 2 + 10 + 2 * 3 * 2.5 - 4 + 1 / 2"))
